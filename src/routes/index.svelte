86a785f09f40b90f9f63c0380a54e842f5c55bf8
Michał Sajnóg <michal.sajnog@remotehq.com>
2022-06-09 20:31:24 +0400

    asd

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,7 @@
 <script context="module">
 	export async function load({ url, fetch }) {
 		const [{ articles, pages }, { tags }] = await Promise.all([
-			fetch(`/articles.json${url.search}`, { credentials: 'include' }).then((r) => r.json()),
+			fetch(`/articles.json${url.search}`, { credentials: undefined }).then((r) => r.json()),
 			fetch('/tags.json').then((r) => r.json())
 		]);
 
